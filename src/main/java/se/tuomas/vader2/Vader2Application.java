37c6d7c6bfdf95e03e42d102288fd2bdaa60c856
Fredrik Tuomas <fredrik.tuomas@gmail.com>
2018-01-26 20:52:06 +0100

    Initial commit

---
+++ src/main/java/se/tuomas/vader2/Vader2Application.java
@@ -0,0 +1,12 @@
+package se.tuomas.vader2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vader2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vader2Application.class, args);
+	}
+}
